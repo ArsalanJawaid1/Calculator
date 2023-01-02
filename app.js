d6c736b98d92174ab5783e1c3a47caa1a529a69f
Arsalan Jawaid <arsaljaved100@gmail.com>
2023-01-03 01:43:15 +0500

    commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// function calc(num){
+//     var a = document.getElementById("input");
+//     a.value += num; 
+// }
+// function clearbtn(){
+//     var a = document.getElementById("input")
+//     a.value = "";
+// }
+// function getResult(){
+//     var a = document.getElementById("input");
+    
+//     a.value = eval(a.value)
+// }
+
+
+// function getNum(num){
+
+//     var a = document.getElementById("get");
+//     a.value += num; 
+   
+// }
+// function clearBtn(){    // you have to clearify the value of clearbtn 
+//     var clear = document.getElementById("get")
+//     clear.value = "";
+// }
+
+// function getResult(){
+//     var a = document.getElementById("get");
+//     a.value = eval(a.value);
+    
+// }
+
+
+
+function getNum(val){
+    var a = document.getElementById('get');
+    a.value += val; 
+}
+
+function clearBtn(){
+    document.getElementById('get').value = "";
+}
+
+function getResult(){
+    var a = document.getElementById('get');
+    a.value = eval(a.value);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
